74f50f7390fdfe4762912c8c4a4d522cf7c69e30
MelakuBi <MelakuBirhanu@gmail.com>
2023-05-13 08:41:55 -0700

    users bluprint

---
+++ models/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' user defination inherited from basemodel'''
+
+from base_model import BaseModel
+
+class User(BaseModel):
+
+        email = ""
+        password = ""
+        first_name = ""
+        last_nanme = ""
+
+        def __init__(self, *args, **kwargs):
+            super.__init__(*args, **kwargs)
+
+
