bb22910590bc61a6126e630e18b8d4a4cd50bbb9
Iman Hamza <iman.ali.hamza@gmail.com>
2023-05-13 07:16:32 -0700

    commenting /models/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 #!/us/bin/python3
 ''' __init_ dundu0r for models module'''
-    from engine.file_storage import Filestorage
-
-    storage = Filestorage()
-    storage.reload()
+# from engine.file_storage import Filestorage
+# storage = Filestorage()
+# storage.reload()
 
