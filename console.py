490e609ad81fa9fbf0351584b0c23466cc6f4171
Iman Hamza <iman.ali.hamza@gmail.com>
2023-05-13 00:29:24 -0700

    1st version of console

--- console.py
+++ console.py
@@ -1,1 +1,27 @@
-#!/usr/bin/python3
+#!/usr/bin/ python3
+'''
+Console entry point of the command interpreter
+'''
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    '''
+    Command interpreter command
+    '''
+
+    # set the promot
+    prompt = '(hbnb)'
+
+    def _quit(self, arg):
+        ''' exit the program '''
+        return True
+
+    def _EOF(self, arg):
+        ''' exit the program '''
+        print()
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
