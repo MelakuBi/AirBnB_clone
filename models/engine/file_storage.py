17bb2740bce739f23e3917406240932cf21e2f5a
Iman Hamza <iman.ali.hamza@gmail.com>
2023-05-13 07:09:19 -0700

    fix pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' define filestorage class 
+''' define filestorage class
 with attributes and methods '''
 import json
 from models.base_model import basemodel
@@ -10,49 +10,47 @@ from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 
+
 class Filestorage:
-    ''' define class attribute and methode 
+    ''' define class attribute and method
     for storage objects'''
+
     __file_path = "file.json"
     _objects = {}
 
     def all(self):
-        #return objects
-       return self._objects
-    
+        # return objects
+        return self._objects
+
     def new(self, obj):
         key = obj.__class__.__name__ + '.' + obj.id
         __objectcts[key] = obj
 
-    def save(self)
+    def save(self):
+        converted = {}
+        for key, value in self.__objects.items():
+            converted[key] = value.to_dict()
+            with open(self.__file_path, w) as jfile:
+                jfile = json.dumps(converted)
 
-    converted = {}
-    for key, value in self.__objects.items():
-        converted[key] = value.to_dict()
-    with open(self.__file_path, w) jfile
-       jfile =  json.dumps(converted)
-
-    def reload(self)
-     ''' deserializes the JSON file to __objects 
-     (only if the JSON file (__file_path) exists ; otherwise, do nothing.
-     If the file doesn’t exist, no exception should be raised)'''
-
+    def reload(self):
+        ''' deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists ; otherwise, do nothing.
+        If the file doesn’t exist, no exception should be raised)'''
         try:
-            with open(self.__file_path,'r') as jfile
+            with open(self.__file_path, 'r') as jfile:
                 pfile = json.load(jfile)
         except Exception:
             return
         objects = eval(pfile)
-            for key, val in objects.items():
-                objects[key] = eval(key.split('.')[0] + '(**value)')
-                self.__objects = objects
+        for key, val in objects.items():
+            objects[key] = eval(key.split('.')[0] + '(**value)')
+            self.__objects = objects
 
-        def delete(self, obj)
+        def delete(self, obj):
             try:
                 key = obj.__class__.__name__ + '.' + str(obj.id)
                 del self.__objects[key]
                 return True
             except Exception:
                 return False
-
-
