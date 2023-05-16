d5d7a4684f7e8a674ec153e094070e5dfb452bfb
MelakuBi <MelakuBirhanu@gmail.com>
2023-05-16 06:06:33 -0700

    include models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,61 +1,85 @@
 #!/usr/bin/python3
-''' define filestorage class
-with attributes and methods '''
-import json
-import os.path
+"""
+module for serializing and deserializing object to file storage
+"""
+#import sys
+#sys.path.append('..')
+#import os
+from models.amenity import Amenity
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
 from models.city import City
 from models.place import Place
-from models.amenity import Amenity
 from models.review import Review
+from models.state import State
+from models.user import User
 
 
-class FileStorage:
-    ''' define class attribute and method
-    for storage objects'''
+class FileStorage():
+    """
+    FileStorage class for serializing and deserializing objects
+    into and from files respectively
+    """
+    engine_directory = os.path.dirname(os.path.abspath(__file__))
+    parent_directory = os.getcwd()
+    __file_path = parent_directory + '/file.json'
+    __objects = dict()
 
-    __file_path = "file.json"
-    __objects = {}
+    def __init__(self):
+        """instantiation method for class
+        """
+        pass
 
     def all(self):
-        # return objects
+        """returns the dictionary __objects
+        """
         return self.__objects
 
     def new(self, obj):
-        key = obj.__class__.__name__ + '.' + str(obj.id)
+        """sets in __objects the obj with key <obj class name>.id
+        """
+        # TODO not sure if this check is needed for holberton checker
+        try:
+            obj_d = obj.to_dict()
+        except:
+            raise TypeError('object passed to filestorage has no to_dict()')
+        key = obj_d['__class__'] + '.' + str(obj_d['id'])
         self.__objects[key] = obj
 
+    def delete(self, obj):
+        """deletes obj from __objects
+        """
+        try:
+            key = obj.__class__.__name__ + '.' + str(obj.id)
+            del self.__objects[key]
+            return True
+        except:
+            return False
+
     def save(self):
-        ob = FileStorage.__objects
-        obd = {obj: ob[obj].to_dict() for obj in ob.keys()}
-        with open(FileStorage.__file__path, 'w') as f:
-            json.dump(obd, f)
+        """serializes the __objects to the JSON file
+            -> path (__file_path)
+        """
+        json_dump = str({k: v.to_dict() for (k, v) in self.__objects.items()})
+        json_dump = json_dump.replace('\'', '"')
+        with open(self.__file_path, 'w', encoding='utf-8') as myFile:
+            myFile.write(json_dump)
 
     def reload(self):
-        ''' deserializes the JSON file to __objects
-        (only if the JSON file (__file_path) exists ; otherwise, do nothing.
-        If the file doesn’t exist, no exception should be raised)'''
+        """deserializes the JSON file to __objects
+            -> path (__file_path)
+           ONLY if it exists, no exceptions are raised
+        """
         try:
-            with open(Filestorage.__file_path, 'r') as jfile:
-                pfile = json.load(jfile)
-                for key, value in pfile.items():
-                    _name, _id = key.split(".")
-                    objectss = eval(_name)(**value)
-                    Filestorage.__objects[key] = objectss
-        except IOError:
-            pass
+            with open(self.__file_path, 'r', encoding='utf-8') as myFile:
+                my_obj_dump = myFile.read()
+        except:
+            return
+        objects = eval(my_obj_dump)
+        for (k, v) in objects.items():
+            objects[k] = eval(k.split('.')[0] + '(**v)')
+        self.__objects = objects
 
-        # objects = json.loads(json.dumps(pfile))
-        # for key, value in objects.items():
-        # objects[key] = eval(key.split('.')[0] + '(**value)')
-        # self.__objects = objects
-
-        def delete(self, obj):
-            try:
-                key = obj.__class__.__name__ + '.' + str(obj.id)
-                del self.__objects[key]
-                return True
-            except Exception:
-                return False
+    def get_filepath(self):
+        """get filepath for JSON file
+        """
+        return self.__file_path
